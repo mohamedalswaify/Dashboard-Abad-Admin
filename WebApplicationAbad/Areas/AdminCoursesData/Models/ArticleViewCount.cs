8fb2a6713f083137c4ad5fdac800770c4c3ae8eb
Mohamed Alswaify <59081888+mohamedalswaify@users.noreply.github.com>
2024-10-18 17:48:39 +0300

    Delete WebApplicationAbad/Areas/AdminCoursesData directory

--- WebApplicationAbad/Areas/AdminCoursesData/Models/ArticleViewCount.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplicationAbad.Areas.AdminCoursesData.Models
-{
-    public class ArticleViewCount
-    {
-        public int Id { get; set; }
-        public int Count { get; set; }
-    }
-}
